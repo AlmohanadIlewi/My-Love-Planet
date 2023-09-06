c35b603ec29354165b2472916b0bb93f20b41fbe
Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2023-09-06 12:30:18 +0200

    Update

--- Server/Models/place.js
+++ Server/Models/place.js
@@ -1,52 +1,55 @@
-// // Das Mongoose-Schema definiert die Struktur
-// // eines Orts in der Datenbank. Hier werden Titel,
-// // Koordinaten, Details, Bilder, Benutzer-ID und andere Felder festgelegt.
+/* Das Mongoose-Schema definiert die Struktur
+eines Orts in der Datenbank. Hier werden Titel,
+Koordinaten, Details, Bilder, Benutzer-ID und andere Felder festgelegt.
+*/
+/*
+const ortSchema = new mongoose.Schema(
+  {
+    city: {
+      type: String,
+    },
+    postType: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    price: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    Address: {
+      type: String,
+    },
+    zipCode: {
+      type: String,
+    },
+    cloudinary_id: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// 
+const Post = mongoose.model("Post", postSchema);
 
-// const ortSchema = new mongoose.Schema(
-//   {
-//     city: {
-//       type: String,
-//     },
-//     postType: {
-//       type: String,
-//     },
-//     title: {
-//       type: String,
-//     },
-//     category: {
-//       type: String,
-//     },
-//     price: {
-//       type: String,
-//     },
-//     content: {
-//       type: String,
-//     },
-//     image: {
-//       type: String,
-//     },
-//     Address: {
-//       type: String,
-//     },
-//     zipCode: {
-//       type: String,
-//     },
-//     cloudinary_id: {
-//       type: String,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+module.exports = Post;
 
-// const Post = mongoose.model("Post", postSchema);
+*/
 
-// module.exports = Post;
+// Dieser Code definiert ein Mongoose-Schema und ein Mongoose-Modell für die Verwaltung von Ortsdaten in einer MongoDB-Datenbank. 
 
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //MongoDB zu interagieren
 const Schema = mongoose.Schema;
 const ortSchema = new mongoose.Schema(
   {
