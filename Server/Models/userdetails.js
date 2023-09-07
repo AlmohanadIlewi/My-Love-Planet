2fc35208663796384e336cb0f708fdc34f1105cb
Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2023-09-07 22:52:18 +0200

    für Code kommentare geschrieben

--- Server/Models/userdetails.js
+++ Server/Models/userdetails.js
@@ -1,22 +1,20 @@
+
+/* dieser Code ermöglicht die Definition eines Mongoose-Modells für Nutzer (Benutzer), 
+das in anderen Teilen der Anwendung verwendet werden kann, 
+um Nutzerdaten in der MongoDB-Datenbank zu speichern und abzurufen. */ 
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userSchema = new mongoose.Schema(
+const nutzerSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-    
-    },
-    country: {type:String},
-  
-    reminder: {type: Boolean},
-    
-    note: {type: String},
 
-    visit: {type: Boolean},
-    
+    firstName: {type:String},
+    lastName: {type:String},
+    email: {type:String},
 
   },
   { timestamps: true }
+  
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", nutzerSchema);
--- Server/server.js
+++ Server/server.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Place = require('./Models/place')
+const express = require('express'); // Importiere das Express-Framework.
+const mongoose = require('mongoose');  // Importiere das Body-Parser-Modul für die Verarbeitung von Anfragedaten.
+const bodyParser = require('body-parser'); // Importiere das Body-Parser-Modul für die Verarbeitung von Anfragedaten.
+const Place = require('./Models/place') // Importiere Datenbankmodelle für Orte und Ort-Daten.
 const Ort = require('./Models/ortdaten')
-const db = require("./db");
-const cors = require('cors');
+const db = require("./db"); // Importiere die Verbindung zur MongoDB-Datenbank.
+const cors = require('cors'); //  Importiere das CORS-Modul, um Anfragen von verschiedenen Domänen zu ermöglichen.
 
-const app = express();
-const port = 8080;
+const app = express(); // Erstellen eine Express-Anwendung.
+const port = 8080; // Setzen den Server-Port auf 8080.
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors()); // Aktivieren CORS für die Anwendung.
+app.use(bodyParser.json()); // Verwenden Body-Parser für JSON-Anfragen.
 
-// Body Parser Middleware (für POST-Anfragen)
+// Aktiviert Body-Parser für JSON- und URL-kodierte Daten. Body Parser Middleware (für POST-Anfragen)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//  Handlen Fehler in der MongoDB-Verbindung.
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get('/', (req, res) => {
-    res.send('Hello from everywhere');
+    res.send('Hello from everywhere');   //  Definiert eine Route für die Wurzel-URL, die "Hello from everywhere" als Antwort sendet.
 });
 
-app.post('/add', (req, res) => {
+app.post('/add', (req, res) => {    //  Definieren eine Route zum Hinzufügen von Ortsdaten zur Datenbank.
     Place.create ({
-        name: req.body.name,
+        city: req.body.city,
         country : req.body.country,
         reminder: req.body.reminder,
         note:  req.body. note,
@@ -35,28 +36,41 @@ app.post('/add', (req, res) => {
 });
 
 
-app.post('/ortdaten', (req, res) => {
+app.post('/ortdaten', (req, res) => { //  Definieren eine Route zum Hinzufügen von Ort-Daten zur Datenbank.
     Ort.create ({
         zipCode: req.body.zipCode,
         street : req.body.street,
-    }).then((newdaten)=>{res.send(newdaten)})
-    
-    .catch(err => res.send(err))
+    }).then((newdaten)=>{res.send(newdaten)}) // Funktion wird aufgerufen, um das Ergebnis anzuzeigen
+                                    
+        .catch(err => res.send(err))           // Funktion wird aufgerufen, um den Fehler anzuzeigen.
 });
 
-app.get('/allplaces', async (req, res) => {
+
+// Definieren eine Route zum Abrufen aller Orte aus der Datenbank.
+app.get('/allplaces', async (req, res) => {  
     const allplaces = await Place.find({})
     .catch((err) => res.status(500).send("Server Error"));
     res.status(200).json(allplaces);
 })
 
-app.get('/info', async (req, res) => {
-    const bank = await Ort.find({})
-    .catch((err) => res.status(500).send("Server Error"));
-    res.status(200).json(bank);
-})
 
+//  Definieren eine Route zum Löschen eines Ortes anhand seiner ID.
+app.delete('/delete/:id', async (req, res) => {
+try {
+    const place = await Place.findOneAndDelete({ _id: req.params.id });
 
+    if (!place) {
+    return res.status(404).json({ success: false, error: `Place not found` });
+    }
+
+    return res.status(200).json({ success: true, data: place });
+} catch (err) {
+    return res.status(400).json({ success: false, error: err.message });
+}
+});
+
+//  Starten den Server und lasse ihn auf dem angegebenen Port laufen.
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
+4
